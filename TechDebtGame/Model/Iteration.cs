023fba4a23475591cfd9ffe3d902c1f8c221264c
Sara Gowen <sara.gowen@ukho.gov.uk>
2020-05-29 12:12:54 +0100

    Removed non-game elements

---
+++ TechDebtGame/Model/Iteration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechDebtGame.Model
+{
+    public class Iteration
+    {
+        public int Number { get; set; }
+        public int TotalCapacity { get; set; }
+        public int AvailableCapacity { get; set; }
+        public int FeaturePointsComplete { get; set; }
+    }
+}
