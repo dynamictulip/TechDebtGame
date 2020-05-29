b014686d2903be10c0ca912556b440569b3deb01
Sara Gowen <sara.gowen@ukho.gov.uk>
2020-05-29 12:59:31 +0100

    Added iteration cards and grid styling

---
+++ TechDebtGame/Model/IterationCard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechDebtGame.Model
+{
+    public class IterationCard
+    {
+        public int Cost { get; set; }
+        public string Scenario { get; set; }
+        public int Impact { get; set; }
+    }
+}
