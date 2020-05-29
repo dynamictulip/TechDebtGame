a662d6e7cb367a327825ab3ea7eef06de5a57cc4
Sara Gowen <sara.gowen@ukho.gov.uk>
2020-05-29 11:17:25 +0100

    Added game page and blazorise

--- TechDebtGame/Program.cs
+++ TechDebtGame/Program.cs
@@ -1,12 +1,11 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace TechDebtGame
 {
@@ -17,9 +16,19 @@ namespace TechDebtGame
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddTransient(sp => new HttpClient
+                    {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)})
+                .AddBlazorise(options => { options.ChangeTextOnKeyPress = true; })
+                .AddBootstrapProviders()
+                .AddFontAwesomeIcons();
+            ;
 
-            await builder.Build().RunAsync();
+            var host = builder.Build();
+
+            host.Services.UseBootstrapProviders()
+                .UseFontAwesomeIcons();
+
+            await host.RunAsync();
         }
     }
 }
